c1c8d7d4e5243dd24c9021674def25929f1c24b8
DominikDillinger <dominik.dillinger@googlemail.com>
2024-04-19 16:18:16 +0200

    FIX CHG Console output from std::err to std::cout for empty executable

--- emptyExecutable/main.cpp
+++ emptyExecutable/main.cpp
@@ -4,8 +4,26 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+#if __linux__
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
+#endif
 int main(int argc, char *argv[])
 {
+#if __linux__
+    if (isatty(STDOUT_FILENO))
+    {
+        // Open /dev/tty for writing, which represents the terminal
+        int tty_fd = open("/dev/tty", O_WRONLY);
+        if (tty_fd != -1)
+        {
+            // Redirect stdout and stderr to /dev/tty
+            dup2(tty_fd, STDOUT_FILENO);
+            dup2(tty_fd, STDERR_FILENO);
+        }
+    }
+#endif
     QGuiApplication app(argc, argv);
 
     QCoreApplication::addLibraryPath("./");
